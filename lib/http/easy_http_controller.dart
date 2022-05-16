b4ca1096e7ba4e07e1e970218e3ddcb0748a96d5
memtopia <memtopia@gmail.com>
2022-05-16 15:15:06 +0800

    remove no use function

--- lib/http/easy_http_controller.dart
+++ lib/http/easy_http_controller.dart
@@ -161,26 +161,24 @@ abstract class EasyHttpController<T> extends GetxController with StateMixin<T> {
     });
   }
 
-  Future<T> onLoading(Future<T> Function() asyncFunction, {Widget? loadingWidget, Color opacityColor = Colors.black, double? opacity}) async {
+  Future<T> onLoading(Future<T> Function() asyncFunction) async {
     return Get.showOverlay(
       asyncFunction: asyncFunction,
-      opacityColor: opacityColor,
-      opacity: opacity ?? .1,
-      loadingWidget: loadingWidget ??
-          Center(
-            child: Container(
-              width: 120,
-              height: 120,
-              decoration: BoxDecoration(color: Colors.white.withOpacity(.4), borderRadius: BorderRadius.circular(10)),
-              child: const SizedBox(
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: CircularProgressIndicator(strokeWidth: 20),
-                ),
-              ),
+      opacity: .1,
+      loadingWidget: Center(
+        child: Container(
+          width: 120,
+          height: 120,
+          decoration: BoxDecoration(color: Colors.white.withOpacity(.4), borderRadius: BorderRadius.circular(10)),
+          child: const SizedBox(
+            width: 50,
+            height: 50,
+            child: Center(
+              child: CircularProgressIndicator(strokeWidth: 2),
             ),
           ),
+        ),
+      ),
     );
   }
 
