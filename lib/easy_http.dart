9fa0594281bc476acb0f5abfaf09f7131d4cb47d
memtopia <memtopia@gmail.com>
2022-05-16 15:41:14 +0800

    添加普通http方法

--- lib/easy_http.dart
+++ lib/easy_http.dart
@@ -13,7 +13,6 @@ class EasyHttp extends GetConnect {
 
   static EasyHttp? _instance;
 
-
   static late BaseEasyHttpConfig _config;
 
   static final List<RequestModifier> _requestInterceptor = [];
@@ -41,5 +40,77 @@ class EasyHttp extends GetConnect {
 
   static addResponseModifier(ResponseModifier interceptor) {
     _responseInterceptor.add(interceptor);
+  }
+
+  static Future<Response<T>> doGet<T>(
+    String url, {
+    Map<String, String>? headers,
+    String? contentType,
+    Map<String, dynamic>? query,
+    Decoder<T>? decoder,
+  }) {
+    return _instance!.get(
+      url,
+      headers: headers,
+      contentType: contentType,
+      query: query,
+      decoder: decoder,
+    );
+  }
+
+  static Future<Response<T>> doPost<T>(
+    String? url,
+    dynamic body, {
+    String? contentType,
+    Map<String, String>? headers,
+    Map<String, dynamic>? query,
+    Decoder<T>? decoder,
+    Progress? uploadProgress,
+  }) {
+    return _instance!.post(
+      url,
+      body,
+      contentType: contentType,
+      headers: headers,
+      query: query,
+      decoder: decoder,
+      uploadProgress: uploadProgress,
+    );
+  }
+
+  static Future<Response<T>> doPut<T>(
+    String url,
+    dynamic body, {
+    String? contentType,
+    Map<String, String>? headers,
+    Map<String, dynamic>? query,
+    Decoder<T>? decoder,
+    Progress? uploadProgress,
+  }) {
+    return _instance!.put(
+      url,
+      body,
+      contentType: contentType,
+      headers: headers,
+      query: query,
+      decoder: decoder,
+      uploadProgress: uploadProgress,
+    );
+  }
+
+  static Future<Response<T>> doDelete<T>(
+    String url, {
+    Map<String, String>? headers,
+    String? contentType,
+    Map<String, dynamic>? query,
+    Decoder<T>? decoder,
+  }) {
+    return _instance!.delete(
+      url,
+      headers: headers,
+      contentType: contentType,
+      query: query,
+      decoder: decoder,
+    );
   }
 }
